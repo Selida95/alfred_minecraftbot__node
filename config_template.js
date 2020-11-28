aacbc806ec1107f492945585ba1521fd0dc2aaa7
Elyas Page <elyaspage@googlemail.com>
2020-11-28 21:52:05 +0000

    Create config_template.js

---
+++ config_template.js
@@ -0,0 +1,18 @@
+/*
+ * ------------------------------------
+ * A.L.F.R.E.D | Minecraft Bot: Config
+ * ------------------------------------
+ */
+
+ // Define config object
+ var config = {}
+
+ config.minecraft = {}
+ config.minecraft.host = ''
+ config.minecraft.port = '25565'
+ config.minecraft.bot = {}
+ config.minecraft.bot.username = ''
+ config.minecraft.bot.password = ''
+
+ // Export config
+ module.exports = config
