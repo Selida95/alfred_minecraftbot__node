1a11c3c397123064a22c0c338fa8d1317de5b69a
Elyas Page <elyaspage@googlemail.com>
2020-11-28 17:53:27 +0000

    Create index.js, setup minecraft bot basic.

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+ * -----------------------------------
+ * A.L.F.R.E.D | Minecraft Bot: Index
+ * -----------------------------------
+ */
+
+ // Dependencies
+ const mineflayer = require('mineflayer')
+
+ // Config
+ const config = require('./config')
+
+ const bot = mineflayer.createBot({
+   host : config.minecraft.host,
+   port : config.minecraft.port,
+   username : config.minecraft.bot.username,
+   password : config.minecraft.bot.password
+ })
